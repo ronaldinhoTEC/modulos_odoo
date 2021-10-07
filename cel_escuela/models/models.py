ba9bc5ed05ee88d006a0d251d4f791ec537d5b4f
ronaldinho farfan <ronaldinhofarfan@gmail.com>
2021-10-07 03:57:36 -0500

    2 campos agregados desde cel

--- cel_escuela/models/models.py
+++ cel_escuela/models/models.py
@@ -8,5 +8,5 @@ class Escuela(models.Model):
     name = fields.Char("Nombre")
     ie_type = fields.Selection([('nivel1','Inicial'),('nivel2', 'Primaria'),('primaria', 'Secundaria')],'Nivel Educativo') 
     ie_entry_time = fields.Datetime('Hora de entrada', default=fields.Datetime.now, required=False, readonly=False, select=True)
-    ie_students = field.Integer("Numero de estudiantes ")
+    ie_students = field.Integer("cantidad de estudiantes ")
     
