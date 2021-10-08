08a604c196b3d802942df133d77563cdba846173
Ronaldinho <ronaldinhofarfan@gmail.com>
2021-10-08 01:28:13 -0500

    CR campo agregado

--- cel_escuela/models/models.py
+++ cel_escuela/models/models.py
@@ -8,5 +8,5 @@ class Escuela(models.Model):
     name = fields.Char("Nombre")
     ie_type = fields.Selection([('nivel1','Inicial'),('nivel2', 'Primaria'),('primaria', 'Secundaria')],'Nivel Educativo') 
     ie_entry_time = fields.Datetime('Hora de entrada', default=fields.Datetime.now, required=False, readonly=False, select=True)
-    ie_students = fields.Integer("Cantidad de estudiante S")
-    
+    ie_students = fields.Integer("Cantidad de estudiante")
+    ie_turno = fields.Selection([('turno1','Mañana'),('turno2','Tarde'),('turno3','Noche')])
