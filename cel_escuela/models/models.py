bf3fa556e3aa98f99378fc3efe1db66918c88eb4
Ronaldinho Farfan <ronaldinhofarfan@gmail.com>
2021-10-08 01:39:15 -0500

    error corregido

--- cel_escuela/models/models.py
+++ cel_escuela/models/models.py
@@ -9,4 +9,4 @@ class Escuela(models.Model):
     ie_type = fields.Selection([('nivel1','Inicial'),('nivel2', 'Primaria'),('primaria', 'Secundaria')],'Nivel Educativo') 
     ie_entry_time = fields.Datetime('Hora de entrada', default=fields.Datetime.now, required=False, readonly=False, select=True)
     ie_students = fields.Integer("Cantidad de estudiante")
-    ie_turno = fields.Selection([('turno1','Mañana'),('turno2','Tarde'),('turno3','Noche')])
+    ie_turno = fields.Selection([('turno1','Mañana'),('turno2','Tarde'),('turno3','Noche')],'Nivel Educativo')
